2276222fc72c70584774de27b2c4bfab2a683215
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-17 16:51:25 +0200

    add User model

---
+++ GuitarCommerceAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuitarCommerceAPI.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
