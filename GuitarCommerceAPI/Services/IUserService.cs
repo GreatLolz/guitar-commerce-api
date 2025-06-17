e80b9f6cd941b12128d1299d92a52df26e76e01b
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-17 17:38:05 +0200

    add UserService

---
+++ GuitarCommerceAPI/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using GuitarCommerceAPI.Models;
+
+namespace GuitarCommerceAPI.Services
+{
+    public interface IUserService
+    {
+        Task<User?> Register(string username, string password);
+        Task<User?> Login(string username, string password);
+    }
+}
---
+++ GuitarCommerceAPI/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using GuitarCommerceAPI.Data;
+using GuitarCommerceAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuitarCommerceAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly PasswordHasher<User> _passwordHasher;
+        private readonly AppDbContext _db;
+
+        public UserService(AppDbContext db) {
+            _passwordHasher = new PasswordHasher<User>();
+            _db = db;
+        }
+
+        public async Task<User?> Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
