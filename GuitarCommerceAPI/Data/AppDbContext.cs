f51a64dc138f8e42146fc6b8bcce037bca4576fa
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-17 16:56:02 +0200

    add db context

---
+++ GuitarCommerceAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GuitarCommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuitarCommerceAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().HasIndex(u => u.Name).IsUnique();
+            modelBuilder.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(u => u.PasswordHash).IsRequired().HasMaxLength(256);
+        }
+    }
+}
