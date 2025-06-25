a2ecb7f9e6f2a65153a2303bdd14c42bf93a3a29
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-26 01:27:53 +0200

    configure Product properties in the db context

--- GuitarCommerceAPI/Data/AppDbContext.cs
+++ GuitarCommerceAPI/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GuitarCommerceAPI.Models;
+using GuitarCommerceAPI.Models.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace GuitarCommerceAPI.Data
@@ -6,6 +7,7 @@ namespace GuitarCommerceAPI.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -17,6 +19,17 @@ namespace GuitarCommerceAPI.Data
             modelBuilder.Entity<User>().HasIndex(u => u.Name).IsUnique();
             modelBuilder.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(100);
             modelBuilder.Entity<User>().Property(u => u.PasswordHash).IsRequired().HasMaxLength(256);
+
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.ImageUrl).IsRequired().HasMaxLength(2048);
+            modelBuilder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.Description).IsRequired().HasMaxLength(1000);
+            modelBuilder.Entity<Product>().Property(p => p.Price).IsRequired().HasPrecision(18, 2);
+            modelBuilder.Entity<Product>().Property(p => p.Category).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.Brand).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.Stock).IsRequired().HasDefaultValue(0);
+            modelBuilder.Entity<Product>().Property(p => p.Rating).IsRequired().HasPrecision(3, 2).HasDefaultValue(0);
+            modelBuilder.Entity<Product>().Property(p => p.ReviewsCount).IsRequired().HasDefaultValue(0);
         }
     }
 }
