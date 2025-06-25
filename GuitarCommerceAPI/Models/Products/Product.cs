0731a8f9ab2e846cdf8776ddf7558bc523b4c203
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-26 01:14:19 +0200

    add Product model

---
+++ GuitarCommerceAPI/Models/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuitarCommerceAPI.Models.Products
+{
+    [Table("Products")]
+    public class Product
+    {
+        public required string Id { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required decimal Price { get; set; }
+        public required string Category { get; set; }
+        public required string Brand { get; set; }
+        public required int Stock { get; set; }
+        public required decimal Rating { get; set; }
+        public required int ReviewsCount { get; set; }
+    }
+}
