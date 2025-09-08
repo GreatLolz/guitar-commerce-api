8645bed6bddb698f6dd2e981e4faf99d6ec4a413
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-08 18:42:06 +0200

    add missing migration

---
+++ GuitarCommerceAPI/Migrations/20250908162549_AddNextItemId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuitarCommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNextItemId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NextItemId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NextItemId",
+                table: "Carts");
+        }
+    }
+}
