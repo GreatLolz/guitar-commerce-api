41a23f19dbd20b6c092f0da7b2fdcd79796135f9
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-08 16:37:26 +0200

    add cart models

---
+++ GuitarCommerceAPI/Models/Cart/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuitarCommerceAPI.Models.Cart
+{
+    [Table("Carts")]
+    public class Cart
+    {
+        public required string Id { get; set; }
+        public required string UserId { get; set; }
+        public required bool IsActive { get; set; }
+
+        public required User User { get; set; }
+        public required ICollection<CartItem> Items { get; set; }
+    }
+}
---
+++ GuitarCommerceAPI/Models/Cart/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using GuitarCommerceAPI.Models.Products;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuitarCommerceAPI.Models.Cart
+{
+    [Table("CartItems")]
+    public class CartItem
+    {
+        public required int Id { get; set; }
+        public required string CartId { get; set; }
+        public required string ProductId { get; set; }
+        public required int Quantity { get; set; }
+
+        public required Cart Cart { get; set; }
+        public required Product Product { get; set; }
+    }
+}
