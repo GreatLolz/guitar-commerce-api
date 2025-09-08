51432ddf5419315f6b8db4c77c6a1f40d1ffddab
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-08 16:43:52 +0200

    create ICartService interface

---
+++ GuitarCommerceAPI/Services/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using GuitarCommerceAPI.Models.Cart;
+
+namespace GuitarCommerceAPI.Services
+{
+    public interface ICartService
+    {
+        Task<Cart> GetCart(string userId);
+        Task AddCartItem(string cartId, string productId);
+        Task DeleteCartItem(string cartId, int itemId);
+    }
+}
