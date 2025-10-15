9577822013c6d923572379c1075e7b63c5782bc2
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-15 15:44:26 +0200

    include order status migrations

---
+++ GuitarCommerceAPI/Migrations/20251015134401_OrderStatus.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuitarCommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Orders",
+                newName: "PaymentStatus");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeliveryStatus",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryStatus",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentStatus",
+                table: "Orders",
+                newName: "Status");
+        }
+    }
+}
